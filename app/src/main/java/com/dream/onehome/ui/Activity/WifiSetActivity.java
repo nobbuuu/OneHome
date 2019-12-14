31f7746bfd57bd9fab019fa45f8378e5e2abf142
li.xy <1184824553@qq.com>
2019-12-14 12:01:10 +0800

    add some xml

---
+++ app/src/main/java/com/dream/onehome/ui/Activity/WifiSetActivity.java
@@ -0,0 +1,34 @@
+package com.dream.onehome.ui.Activity;
+
+import com.dream.onehome.base.BaseActivity;
+
+/**
+ * Time:2019/12/14
+ * Author:TiaoZi
+ */
+public class WifiSetActivity extends BaseActivity {
+    @Override
+    public int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void OnResume() {
+
+    }
+
+    @Override
+    public void loadDatas() {
+
+    }
+
+    @Override
+    public void eventListener() {
+
+    }
+}
